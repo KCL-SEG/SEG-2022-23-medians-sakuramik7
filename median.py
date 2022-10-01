c5efa83f44667306eedca76234f5a00bad556589
sakuramik7 <99272370+sakuramik7@users.noreply.github.com>
2022-10-01 12:10:54 +0100

    Algorithm for median

--- median.py
+++ median.py
@@ -5,6 +5,15 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+
+        numbers.sort()
+        if len(numbers) % 2 == 1:
+            print(numbers[int((len(numbers)+1)/2)-1])
+        else:
+            num = numbers[int((len(numbers)-1)/2)]
+            num2 = numbers[int((len(numbers)+1)/2)]
+            a = (num + num2)/2
+            print(a)
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
